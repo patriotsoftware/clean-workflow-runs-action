37a53f06b8b5c1de6f779f61682f1581e13664e6
Marco <maguilar@patriotsoftware.com>
2024-08-01 11:16:51 -0400

    test days parameter

--- delete-runs.sh
+++ delete-runs.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 
-echo "Getting all completed runs for workflow $WORKFLOW_NAME in $REPOSITORY"
+if [[-z "$WORKFLOW_NAME"]]
+then
+    date=$(date --date='$DAYS_AGO days ago' --iso-8601='seconds')
+    echo "Getting all workflows in $REPOSITORY older than $DAYS_AGO or before $date"
+    
+    RUNS=$(
+      gh api \
+        -H "Accept: application/vnd.github+json" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        "/repos/$REPOSITORY/actions/workflows/$WORKFLOW_NAME/runs" \
+        --paginate \
+        --jq '.workflow_runs[] | select(.created_at < "'$date'") | .id'
+    ) 
+else
+    echo "Getting all completed runs for workflow $WORKFLOW_NAME in $REPOSITORY"
 
-RUNS=$(
-  gh api \
-    -H "Accept: application/vnd.github+json" \
-    -H "X-GitHub-Api-Version: 2022-11-28" \
-    "/repos/$REPOSITORY/actions/workflows/$WORKFLOW_NAME/runs" \
-    --paginate \
-    --jq '.workflow_runs[] | select(.conclusion != "") | .id'
-) 
+    RUNS=$(
+      gh api \
+        -H "Accept: application/vnd.github+json" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        "/repos/$REPOSITORY/actions/workflows/$WORKFLOW_NAME/runs" \
+        --paginate \
+        --jq '.workflow_runs[] | select(.conclusion != "") | .id'
+    ) 
 
-echo "Found $(echo "$RUNS" | wc -l) completed runs for workflow $WORKFLOW_NAME"
+    echo "Found $(echo "$RUNS" | wc -l) completed runs for workflow $WORKFLOW_NAME"
+fi
 
 for RUN in $RUNS; do
-  gh run delete --repo $REPOSITORY $RUN || echo "Failed to delete run $RUN"
+  # gh run delete --repo $REPOSITORY $RUN || echo "Failed to delete run $RUN"
   sleep 0.1
 done
