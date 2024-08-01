e9a52e38d3f82c7c7bce51f6c722bf033899a28e
Marco <maguilar@patriotsoftware.com>
2024-08-01 12:46:41 -0400

    format date

--- delete-runs.sh
+++ delete-runs.sh
@@ -3,7 +3,7 @@
 if [ -z "${WORKFLOW_NAME}" ]
 then
     # date=$(date --date='$DAYS_AGO days ago' --iso-8601='seconds')
-    date=$(date -d @${DAYS_AGO} + " days ago" +%s)
+    date=$(date -d $DAYS_AGO + " days ago" +%s)
     formatted_date=$(date -d @${date} +'%Y-%m-%d %H:%M:%S')
     echo "Getting all workflows in $REPOSITORY older than $DAYS_AGO or before $formatted_date"
     
